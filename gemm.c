6699ff086d79c94cab3396e6cc791847c901e8c2
michbogos <bogostamicheal@gmail.com>
2024-08-01 17:41:54 +0200

    Work on reading the test file

--- gemm.c
+++ gemm.c
@@ -1,22 +1,53 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(){
-    float mat1[4][4] = {{3.0f, 0.0f, 0.0f, 0.0f}, {0.0f, 3.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 3.0f, 0.0f}, {0.0f, 0.0f, 0.0f, 3.0f}};
-    float mat2[4][4] = {{2.0f, 0.0f, 0.0f, 0.0f}, {0.0f, 2.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 2.0f, 0.0f}, {0.0f, 0.0f, 0.0f, 2.0f}};
-    float mat3[4][4] = {{0.0f, 0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f, 0.0f}};
+    int N, M, K;
 
-    for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 4; j++){
-            for(int k = 0; k < 4; k++){
-                mat3[j][i] += mat1[j][k]*mat2[k][i];
-            }
-        }
+    FILE* fileptr;
+
+    fileptr = fopen("tst.txt", "r");  // Open the file in binary mode
+    // fseek(fileptr, 0, SEEK_END);          // Jump to the end of the file
+    // filelen = ftell(fileptr);             // Get the current byte offset in the file
+    // rewind(fileptr);                      // Jump back to the beginning of the file
+
+    // buffer = (char *)malloc(filelen * sizeof(char)); // Enough memory for the file
+    // fread(buffer, filelen, 1, fileptr); // Read in the entire file
+    // fclose(fileptr); // Close the file
+
+    int res = fscanf(fileptr, "%d %d %d", &N, &M, &K);
+    if(res != 3) return 0;
+    printf("%d %d %d\n", N, M, K);
+    float mat1[3*N*M];
+    float mat2[M*K];
+    float mat3[N][K];
+
+    char buf[256];
+
+    for(int i = 0; i < 3*N*M; i++){
+        fscanf(fileptr, "%f", mat1+i);
     }
-    for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 4; j++){
-            printf("%1.1f ", mat3[i][j]);
-        }
-        printf("\n");
-    }
+
+    // for(int i = 0; i < M*K; i++){
+    //     fgets(buf, 256, fileptr);
+    //     sscanf(buf, "%f", mat2+i);
+    // }
+
+
+    // for(int i = 0; i < N; i++){
+    //     for(int j = 0; j < M; j++){
+    //         for(int k = 0; k < K; k++){
+    //             mat3[i][k] += mat1[i*M+j]*mat2[j*K+k];
+    //         }
+    //     }
+    // }
+    // for(int i = 0; i < N; i++){
+    //     for(int j = 0; j < K; j++){
+    //         printf("%1.1f ", mat3[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    fclose(fileptr);
     return 0;
 }
--- testgen.py
+++ testgen.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 N = 1024
-M = 512
+M = 1024
 K = 1024
 
 mat1 = np.random.rand(N, M)
@@ -11,10 +11,10 @@ mat3 = mat1@mat2
 with open("test.txt", "w") as f:
     f.write(" ".join([str(i) for i in [N, M, K]])+"\n")
     for i in range(N):
-        f.write(" ".join([str(mat1[i][j]) for j in range(M)])+"\n")
+        f.write("\n".join([str(mat1[i][j]) for j in range(M)])+"\n")
     
     for i in range(M):
-        f.write(" ".join([str(mat2[i][j]) for j in range(K)])+"\n")
+        f.write("\n".join([str(mat2[i][j]) for j in range(K)])+"\n")
     
     for i in range(N):
-        f.write(" ".join([str(mat3[i][j]) for j in range(K)])+"\n")
+        f.write("\n".join([str(mat3[i][j]) for j in range(K)])+"\n")
