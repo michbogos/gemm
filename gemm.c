c6b3cceb69ca4f009780e8bb1cea2f3f508e5e1f
michbogos <bogostamicheal@gmail.com>
2024-08-01 22:57:06 +0200

    Working comparison to reference matmul

--- gemm.c
+++ gemm.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 int main(){
     int N, M, K;
@@ -26,7 +27,8 @@ int main(){
     printf("%d %d %d\n", N, M, K);
     float mat1[N*M];
     float mat2[M*K];
-    float mat3[N][K];
+    float mat3[N*K];
+    float matref[N*K];
 
     for(int i = 0; i < N*M; i++){
         fscanf(fileptr, "%f", &mat1[i]);
@@ -36,23 +38,26 @@ int main(){
         fscanf(fileptr, "%f", &mat2[i]);
     }
 
+    for(int i = 0; i < N*K; i++){
+        fscanf(fileptr, "%f", &matref[i]);
+    }
+
 
     for(int i = 0; i < N; i++){
         for(int j = 0; j < M; j++){
             for(int k = 0; k < K; k++){
-                mat3[i][k] += mat1[i*M+j]*mat2[j*K+k];
+                mat3[i*K+k] += mat1[i*M+j]*mat2[j*K+k];
             }
         }
     }
-    // for(int i = 0; i < N; i++){
-    //     for(int j = 0; j < K; j++){
-    //         printf("%1.1f ", mat3[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-
-    printf("%f\n", mat3[1023][1023]);
-
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < K; j++){
+            if(fabsf(mat3[i*K+j]-matref[i*K+j]) > 0.001){
+                printf("Matmul error: %f %f\n", mat3[i*K+j], matref[i*K+j]);
+                return 1;
+            }
+        }
+    }
     fclose(fileptr);
     return 0;
 }
