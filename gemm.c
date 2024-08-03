5a0368c85b33c5f815410a79d769cf7744096c78
michbogos <bogostamicheal@gmail.com>
2024-08-03 19:36:22 +0200

    C internal random floats, regression to 5 GFLOPS/S

--- gemm.c
+++ gemm.c
@@ -4,38 +4,51 @@
 #include<time.h>
 #include<immintrin.h>
 
+float randf(){
+    return (float)rand()/(float)RAND_MAX;
+}
+
 int main(){
     int N, M, K;
 
-    FILE* fileptr;
+    N = 512;
+    M = 512;
+    K = 512;
 
-    fileptr = fopen("test.txt", "r");  // Open the file in binary mode
-    if(fileptr == NULL){
-        return 0;
-    }
-    int res = fscanf(fileptr, "%d %d %d", &N, &M, &K);
-    if(res != 3) return 0;
+    // FILE* fileptr;
+
+    // fileptr = fopen("test.txt", "r");  // Open the file in binary mode
+    // if(fileptr == NULL){
+    //     return 0;
+    // }
+    // int res = fscanf(fileptr, "%d %d %d", &N, &M, &K);
+    // if(res != 3) return 0;
     printf("%d %d %d\n", N, M, K);
-    float mat1[N*M];
-    float mat2[M*K];
-    float mat3[N*K];
-    float matref[N*K];
+    float* mat1 =   aligned_alloc(32, N*M*sizeof(float));
+    float* mat2 =   aligned_alloc(32, M*K*sizeof(float));
+    float* mat3 =   aligned_alloc(32, N*K*sizeof(float));
+    float* matref = aligned_alloc(32, N*K*sizeof(float));
 
     for(int i = 0; i < N*M; i++){
-        fscanf(fileptr, "%f", &mat1[i]);
+        // fscanf(fileptr, "%f", &mat1[i]);
+        mat1[i] = randf();
     }
 
     for(int i = 0; i < M*K; i++){
-        fscanf(fileptr, "%f", &mat2[i]);
+        // fscanf(fileptr, "%f", &mat2[i]);
+        mat2[i] = randf();
     }
 
-    for(int i = 0; i < N*K; i++){
-        fscanf(fileptr, "%f", &matref[i]);
+    // for(int i = 0; i < N*K; i++){
+    //     // fscanf(fileptr, "%f", &matref[i]);
+    // }
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            for(int k = 0; k < K; k++){
+                matref[i*K+k] += mat1[i*M+j]*mat2[j*K+k];
+            }
+        }
     }
-
-    __m256 a;
-    __m256 b;
-    __m256 c;
 
 
     clock_t tic, toc;
@@ -59,6 +72,6 @@ int main(){
         }
     }
     printf("GFLOPS/S: %f\n", N*K*M/(((float)toc-(float)tic)/(float)CLOCKS_PER_SEC)/1000000000);
-    fclose(fileptr);
+    // fclose(fileptr);
     return 0;
 }
